e386c36d9ef1e3d5f951239c9ca5c3e44e8a0f9a
yuqiu314 <yuqiu314@hotmail.com>
2019-04-25 18:52:24 +0800

    created a simple http server

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 Using Travis-CI and Heroku to automate CI/CD
 
-## 
+## Steps here
+1. Create an empty repository
+2. Add main.go file
+3. Run "go mod init github.com/yuqiu314/cicd-with-golang"
+4. Add 2 GET in main file
+5. 
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, fmt.Sprintf("London bridge is falling down"))
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
